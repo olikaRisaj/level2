6a045e529066d78b6c283261357f1385e971423c
khorbach <kostiantyn.horbach@nordstrom.com>
2021-12-13 13:11:25 +0200

    added viewmodel, moved list generating to viewmodel

--- app/src/main/java/com/example/applesson2/AppViewModel.kt
+++ app/src/main/java/com/example/applesson2/AppViewModel.kt
@@ -4,13 +4,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AppViewModel: ViewModel() {
-    val contactsList: MutableLiveData<List<Contact>> by lazy {
-        MutableLiveData<List<Contact>>().also { loadUsers() }
+    val contactsList: MutableLiveData<List<Contact>> = MutableLiveData()
+
+    init {
+        if(contactsList.value == null) {
+            contactsList.value = loadUsers()
+        }
     }
 
-    fun getContacts() = contactsList
 
-    fun loadUsers() {}
-
-
+    fun loadUsers(): List<Contact> {
+        return listOf(Contact("https://adobe.ly/3rdh0Hw", "Contact 1", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 2", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 3", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 4", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 5", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 6", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 7", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 8", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 9", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 10", "profession"),
+            Contact("https://adobe.ly/3rdh0Hw","Contact 11", "profession"), )
+    }
 }
--- app/src/main/java/com/example/applesson2/MainActivity.kt
+++ app/src/main/java/com/example/applesson2/MainActivity.kt
@@ -2,34 +2,25 @@ package com.example.applesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-
-    private var layoutManager: RecyclerView.LayoutManager? = null
+    private val appViewModel: AppViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val contacts = listOf(Contact("https://adobe.ly/3rdh0Hw", "Contact 1", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 2", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 3", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 4", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 5", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 6", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 7", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 8", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 9", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 10", "profession"),
-            Contact("https://adobe.ly/3rdh0Hw","Contact 11", "profession"), )
-
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerViewXml)
-        layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = RecyclerAdapter(contacts)
+        recyclerView.adapter = RecyclerAdapter(emptyList())
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
+        appViewModel.contactsList.observe(this, {list ->
+            recyclerView.adapter = RecyclerAdapter(list)
+        })
     }
 }
